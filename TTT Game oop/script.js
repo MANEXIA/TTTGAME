faf0a247fe959ee9b27a0f5d08b03cb0218d56ed
Martin Gebiertas <114209897+MANEXIA@users.noreply.github.com>
2023-04-15 08:18:00 +0700

    Add files via upload

---
+++ TTT Game oop/script.js
@@ -0,0 +1,115 @@
+"user-strict";
+ const winningOutcomes = [
+   //HORIZONTAL
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+   //VERTICAL
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+
+   //SLANT AHSHAHSHA
+    [0,4,8],
+    [2,4,6]
+  ]
+
+const p1 = []
+const p2 = []
+let p1score = document.getElementById('p1score')
+let p2score = document.getElementById('p2score')
+//EVENT USER CLICK
+let stats = document.getElementById('status')
+const bx = document.querySelectorAll(".box")
+
+let crntplayer = "Xplayer"
+
+
+function resetArrNbox(){
+   p1.length = 0;
+   p2.length = 0;
+   bx.forEach((elbox) => {
+      elbox.style.backgroundImage = null;
+   })
+}
+
+
+
+bx.forEach((v, i, arr) => {
+      const box = v;
+      box.addEventListener("click", (e) => {
+         const valbox = e.target.dataset.iubox
+         //DO THINGS IN X SYITTTT
+         if(box.style.backgroundImage == '' && crntplayer == "Xplayer"){
+          
+            box.style.backgroundImage = "url('img/x.png')";
+            crntplayer = "Oplayer" //PLAYER STATE
+            p1.push(Number(valbox)) //PUSH INPUTS
+
+            //PROMT OUTCOME
+            const p1result = winningOutcomes.some((winningOutcome) => {
+               return winningOutcome.every((el) => {
+                  //return p1.includes(el)
+                  return p1.includes(el)
+               })
+            })
+
+              
+            if(p1result){
+               document.getElementById('outcome').innerHTML = "PLAYER X WIN!!"
+               p1score.innerHTML = Number(p1score.innerHTML) + 1;
+               resetArrNbox()
+            
+            }else if(p1.length + p2.length == 9){
+               document.getElementById('outcome').innerHTML = "DRAW!!"
+               resetArrNbox()
+            }
+
+            document.getElementById('status').innerHTML = "Player O" 
+
+         //DO THINGS IN O SYITTTT
+         }else if(box.style.backgroundImage == '' && crntplayer == "Oplayer"){
+
+            box.style.backgroundImage = "url('img/OOOO.png')";
+            crntplayer = "Xplayer"  //PLAYER STATE
+            p2.push(Number(valbox)) //PUSH INPUTS
+
+            //PROMT OUTCOME
+            const p2result = winningOutcomes.some((winningOutcome) => {
+               return winningOutcome.every((el) => {
+                  return p2.includes(el);
+               })
+            })
+
+            // console.log(p2result)
+            if(p2result){
+               document.getElementById('outcome').innerHTML = "PLAYER O WIN!!"           
+               p2score.innerHTML = Number(p2score.innerHTML) + 1;
+               resetArrNbox()
+         
+            }else if(p1.length + p2.length == 9){
+               document.getElementById('outcome').innerHTML = "DRAW!!"
+               resetArrNbox()
+            }
+               document.getElementById('status').innerHTML = "Player X" 
+         }
+      })
+
+}) 
+
+document.getElementById('resetbtn').onclick = () =>{
+   document.getElementById('outcome').innerHTML = ""
+   p1score.innerHTML = ""
+   p2score.innerHTML = ""
+   resetArrNbox()
+}
+
+
+// bx.forEach((elbox) => {
+//    // console.log(elbox.style.backgroundImage)
+//    if(elbox.style.backgroundImage){
+//       console.log("meron")
+//    }else{
+//       console.log("wala")
+//    }
+// })
